e64d7dcfddce1a009f739fd36bd3ea16b1aa4503
mark <728239@gmail.com>
2018-11-06 13:23:55 +0300

    Рест метод всех устройств зареган в HAL-браузере

---
+++ monitoring-backend/src/main/java/com/lukdut/monitoring/backend/repository/Devices.java
@@ -0,0 +1,20 @@
+package com.lukdut.monitoring.backend.repository;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+
+import java.util.List;
+
+public class Devices extends ResourceSupport {
+    private final List<Long> imeis;
+
+    @JsonCreator
+    public Devices(@JsonProperty("imeis") List<Long> imeis) {
+        this.imeis = imeis;
+    }
+
+    public List<Long> getContent() {
+        return imeis;
+    }
+}
--- monitoring-backend/src/main/java/com/lukdut/monitoring/backend/rest/SensorsRest.java
+++ monitoring-backend/src/main/java/com/lukdut/monitoring/backend/rest/SensorsRest.java
@@ -1,22 +1,37 @@
 package com.lukdut.monitoring.backend.rest;
 
 import com.lukdut.monitoring.backend.repository.DataRepository;
+import com.lukdut.monitoring.backend.repository.Devices;
+import org.springframework.data.rest.webmvc.RepositoryLinksResource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
-public class SensorsRest {
-
+public class SensorsRest implements ResourceProcessor<RepositoryLinksResource> {
+    private static final String ALL_DEVICES = "allDevices";
     private final DataRepository repository;
 
-    public SensorsRest(DataRepository repository){
+    public SensorsRest(DataRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/allDevices")
-    public List<Long> allDevices(){
-        return repository.findAllDistinctImei();
+    @GetMapping("/" + ALL_DEVICES)
+    public HttpEntity<Devices> allDevices() {
+        Devices devices = new Devices(repository.findAllDistinctImei());
+        devices.add(linkTo(methodOn(SensorsRest.class).allDevices()).withSelfRel());
+        return new ResponseEntity<>(devices, HttpStatus.OK);
+    }
+
+    @Override
+    public RepositoryLinksResource process(RepositoryLinksResource resource) {
+        resource.add(linkTo(methodOn(SensorsRest.class).allDevices()).withRel(ALL_DEVICES));
+        return resource;
     }
 }
