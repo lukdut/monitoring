d19016ff2fde6797c46fe05bfe048f13af326ec0
mark <728239@gmail.com>
2018-11-12 15:36:44 +0300

    Swagger вместо HALbrowser-а

--- monitoring-backend/src/main/java/com/lukdut/monitoring/backend/MonitoringBackendApplication.java
+++ monitoring-backend/src/main/java/com/lukdut/monitoring/backend/MonitoringBackendApplication.java
@@ -3,14 +3,29 @@ package com.lukdut.monitoring.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @IntegrationComponentScan
 @EnableCaching
+@EnableSwagger2
 public class MonitoringBackendApplication {
-    //TODO: read command response channel
     public static void main(String[] args) {
         SpringApplication.run(MonitoringBackendApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lukdut.monitoring.backend.rest"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
